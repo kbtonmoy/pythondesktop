a4ce98e811d1f2ae5f71a42387024c36244d5c9d
Khabirul Basar <kbtonmoy641@gmail.com>
2023-12-02 00:10:29 +0600

    csv added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter as tk
-from tkinter import messagebox, Canvas, Scrollbar, Frame
+from tkinter import ttk
+from tkinter import filedialog, messagebox
 import mysql.connector
 from mysql.connector import Error
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import csv
 import os
-
+from threading import Thread
+import queue
 
 class DatabaseApp:
     def __init__(self, root):
@@ -14,6 +17,8 @@ class DatabaseApp:
         self.connection_details = {}
         self.connection = None  # Database connection instance variable
         self.create_connection_frame()
+
+# All Frames are here
 
     def create_connection_frame(self):
         self.connection_frame = tk.Frame(self.root)
@@ -37,6 +42,25 @@ class DatabaseApp:
 
         tk.Button(self.connection_frame, text="Connect", command=self.connect_to_database).grid(row=4, columnspan=2,
                                                                                                 pady=10)
+
+    def prepare_videos_frame(self):
+        # Placeholder method for "Prepare Videos" functionality
+        messagebox.showinfo("Info", "Prepare Videos functionality not implemented yet")
+
+    def upload_youtube_frame(self):
+        # Placeholder method for "Upload on YouTube" functionality
+        messagebox.showinfo("Info", "Upload on YouTube functionality not implemented yet")
+
+    def show_success_frame(self):
+        self.create_option_buttons()
+
+    def take_screenshots_frame(self):
+        self.clear_all_widgets()
+        self.create_upload_csv_button()
+
+
+# All Buttons are here
+
     def create_option_buttons(self):
         # New method to create buttons for "Take Screenshots", "Prepare Videos", "Upload on YouTube"
         tk.Button(self.root, text="Take Screenshots", command=self.take_screenshots_frame).pack()
@@ -47,23 +71,11 @@ class DatabaseApp:
         self.screenshot_button = tk.Button(self.root, text="Start Capturing", command=self.take_screenshots)
         self.screenshot_button.pack(pady=10)
 
-    def create_scrollable_frame(self):
-        canvas = Canvas(self.root)
-        scrollbar = Scrollbar(self.root, orient="vertical", command=canvas.yview)
-        self.scrollable_frame = Frame(canvas)
+    def create_upload_csv_button(self):
+        tk.Button(self.root, text="Upload CSV", command=self.upload_csv).pack()
 
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
-            )
-        )
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-        canvas.configure(yscrollcommand=scrollbar.set)
 
-        canvas.pack(side="left", fill="both", expand=True)
-        scrollbar.pack(side="right", fill="y")
-
+# ALl logical Functions are here
     def connect_to_database(self):
         self.connection_details = {
             # 'host': self.host_entry.get(),
@@ -84,63 +96,84 @@ class DatabaseApp:
         except Error as e:
             messagebox.showerror("Error", f"Error connecting to MySQL Database: {e}")
 
-    def show_success_frame(self):
-        self.create_option_buttons()
+    def upload_csv(self):
+        filename = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+        if filename:
+            self.process_csv(filename)
 
-    def take_screenshots(self):
+    def process_csv(self, filename):
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            urls = [row['url'] for row in reader]
+            self.start_screenshot_process(urls)
+
+    def start_screenshot_process(self, urls):
+        self.progress_var = tk.DoubleVar()
+        self.progress_bar = ttk.Progressbar(self.root, variable=self.progress_var, maximum=len(urls))
+        self.progress_bar.pack()
+
+        self.queue = queue.Queue()
+        for url in urls:
+            thread = Thread(target=self.take_screenshots, args=(url, self.queue))
+            thread.start()
+
+        self.root.after(100, self.check_queue)
+
+    def check_queue(self):
+        try:
+            while True:
+                message = self.queue.get_nowait()
+                if message == "done":
+                    self.progress_var.set(self.progress_var.get() + 1)
+        except queue.Empty:
+            pass
+
+        if self.progress_var.get() == self.progress_bar['maximum']:
+            self.progress_bar.destroy()
+            messagebox.showinfo("Info", "Screenshots taken for all URLs and database updated")
+        else:
+            self.root.after(100, self.check_queue)
+
+    def take_screenshots(self, url, queue):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--ignore-certificate-errors")  # Ignore SSL errors
-        chrome_options.add_argument("--start-maximized")  # Start maximized
-        chrome_options.add_argument("--window-size=1920,1080")  # Set window size
-        chrome_options.add_argument("--hide-scrollbars")  # Set window size
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--hide-scrollbars")
         driver = webdriver.Chrome(options=chrome_options)
+        driver.set_page_load_timeout(10)  # Setting a timeout for page load
 
-        os.makedirs('screenshots', exist_ok=True)  # Ensure the screenshot directory exists
+        os.makedirs('screenshots', exist_ok=True)
 
-        for var, url in self.checkboxes:
-            if var.get():
-                try:
-                    if not url.startswith(('http://', 'https://')):
-                        url = 'http://' + url  # Add http:// if not present
-                    driver.get(url)
-                    # Adjust the timeout as needed
-                    driver.implicitly_wait(3)  # Wait for the page to load
-                    screenshot_path = os.path.join('screenshots',
-                                                   f"{url.replace('http://', '').replace('https://', '').replace('/', '_')}.png")
-                    driver.save_screenshot(screenshot_path)
-                except Exception as e:
-                    print(f"Error taking screenshot of {url}: {e}")
 
-        driver.quit()
-        messagebox.showinfo("Info", "Screenshots taken")
+        try:
+            if not url.startswith(('http://', 'https://')):
+                url = 'http://' + url
+            driver.get(url)
+            driver.implicitly_wait(3)  # Adjust the timeout as needed
+            screenshot_filename = f"{url.replace('http://', '').replace('https://', '').replace('/', '_')}.png"
+            screenshot_path = os.path.join('screenshots', screenshot_filename)
+            driver.save_screenshot(screenshot_path)
+            self.update_database(url, screenshot_path)
+            driver.quit()
+            self.queue.put("done")
+        except Exception as e:
+            print(f"Error taking screenshot of {url}: {e}")
+        return True
 
-    def take_screenshots_frame(self):
-        self.clear_all_widgets()
-        self.create_scrollable_frame()
-        self.create_screenshot_button()
-        query = "SELECT id, root_domain FROM ecom_platform1"
-        cursor = self.connection.cursor()
-        cursor.execute(query)
-        self.records = cursor.fetchall()
+    def update_database(self, url, screenshot_path):
+        try:
+            cursor = self.connection.cursor()
+            query = "INSERT INTO url_screenshots (root_domain, location) VALUES (%s, %s) ON DUPLICATE KEY UPDATE location = VALUES(location)"
+            cursor.execute(query, (url, screenshot_path))
+            self.connection.commit()
+            cursor.close()
+        except Error as e:
+            messagebox.showerror("Database Error", f"Error updating the database: {e}")
 
-        self.checkboxes = []
-        for i, (id, root_domain) in enumerate(self.records, start=1):
-            tk.Label(self.scrollable_frame, text=f"{id}: {root_domain}").grid(row=i, column=0)
-            var = tk.BooleanVar()
-            chk = tk.Checkbutton(self.scrollable_frame, variable=var)
-            chk.grid(row=i, column=1)
-            self.checkboxes.append((var, root_domain))
 
-        cursor.close()
-
-    def prepare_videos_frame(self):
-        # Placeholder method for "Prepare Videos" functionality
-        messagebox.showinfo("Info", "Prepare Videos functionality not implemented yet")
-
-    def upload_youtube_frame(self):
-        # Placeholder method for "Upload on YouTube" functionality
-        messagebox.showinfo("Info", "Upload on YouTube functionality not implemented yet")
+# Global Functions are here
 
     def clear_all_widgets(self):
 
@@ -152,3 +185,21 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = DatabaseApp(root)
     root.mainloop()
+
+
+ # def create_scrollable_frame(self):
+    #     canvas = Canvas(self.root)
+    #     scrollbar = Scrollbar(self.root, orient="vertical", command=canvas.yview)
+    #     self.scrollable_frame = Frame(canvas)
+    #
+    #     self.scrollable_frame.bind(
+    #         "<Configure>",
+    #         lambda e: canvas.configure(
+    #             scrollregion=canvas.bbox("all")
+    #         )
+    #     )
+    #     canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+    #     canvas.configure(yscrollcommand=scrollbar.set)
+    #
+    #     canvas.pack(side="left", fill="both", expand=True)
+    #     scrollbar.pack(side="right", fill="y")
