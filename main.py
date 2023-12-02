6b1f32f3950e6508426da1dda19947cf2ba0c2a7
Khabirul Basar <kbtonmoy641@gmail.com>
2023-12-02 21:52:37 +0600

    Bug fixes

--- main.py
+++ main.py
@@ -279,23 +279,22 @@ class DatabaseApp:
 
     def open_video_preparation_frame(self):
         # New window (or frame) for video preparation settings
-        self.prep_window = tk.Toplevel(self.root)
-        self.prep_window.title("Video Preparation Settings")
+        self.clear_all_widgets()
 
         # Input field for video file
-        tk.Label(self.prep_window, text="Select a video file:").pack()
+        tk.Label(self.root, text="Select a video file:").pack()
         self.video_path_var = tk.StringVar()
-        tk.Entry(self.prep_window, textvariable=self.video_path_var, state='readonly').pack()
-        tk.Button(self.prep_window, text="Browse", command=self.select_video_file).pack()
+        tk.Entry(self.root, textvariable=self.video_path_var, state='readonly').pack()
+        tk.Button(self.root, text="Browse", command=self.select_video_file).pack()
 
         # Input field for export directory
-        tk.Label(self.prep_window, text="Select export directory:").pack()
+        tk.Label(self.root, text="Select export directory:").pack()
         self.export_dir_var = tk.StringVar()
-        tk.Entry(self.prep_window, textvariable=self.export_dir_var, state='readonly').pack()
-        tk.Button(self.prep_window, text="Browse", command=self.select_export_directory).pack()
+        tk.Entry(self.root, textvariable=self.export_dir_var, state='readonly').pack()
+        tk.Button(self.root, text="Browse", command=self.select_export_directory).pack()
 
         # Submit button
-        tk.Button(self.prep_window, text="Submit", command=self.save_video_settings).pack()
+        tk.Button(self.root, text="Submit", command=self.save_video_settings).pack()
 
     def select_video_file(self):
         # Open file dialog to select a video file
@@ -313,7 +312,6 @@ class DatabaseApp:
         # Save the video path and export directory
         self.video_path = self.video_path_var.get()
         self.export_dir = self.export_dir_var.get()
-        self.prep_window.destroy()
         self.prepare_videos_frame()
 # Global Functions are here
 
